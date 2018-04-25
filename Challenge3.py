768aa8d200002236bfa43287d2d0b07a1c338788
Daniel Murberg <daniel.murberg@hyperisland.se>
2018-04-25 16:31:10 +0200

    add

--- Challenge3.py
+++ Challenge3.py
@@ -1,1 +1,51 @@
-import Challenge1
+freqs = {
+    'a': 0.0651738,
+    'b': 0.0124248,
+    'c': 0.0217339,
+    'd': 0.0349835,
+    'e': 0.1041442,
+    'f': 0.0197881,
+    'g': 0.0158610,
+    'h': 0.0492888,
+    'i': 0.0558094,
+    'j': 0.0009033,
+    'k': 0.0050529,
+    'l': 0.0331490,
+    'm': 0.0202124,
+    'n': 0.0564513,
+    'o': 0.0596302,
+    'p': 0.0137645,
+    'q': 0.0008606,
+    'r': 0.0497563,
+    's': 0.0515760,
+    't': 0.0729357,
+    'u': 0.0225134,
+    'v': 0.0082903,
+    'w': 0.0171272,
+    'x': 0.0013692,
+    'y': 0.0145984,
+    'z': 0.0007836,
+    ' ': 0.1918182 
+}
+
+def xor(A, B):
+    return b''.join(bytes([A[i]^B[i]]) for i in range(len(B)))
+
+
+def score(plain):
+    score = 0
+    
+    for i in plain:
+        if chr(i) in freqs:
+            score +=freqs[chr(i)]
+    return score
+
+def find_single_byte_key(cipher):
+    scores = [score(xor(cipher, bytes([i]*len(cipher)))) for i in range(255)]
+    return bytes([scores.index(max(scores))])
+        
+
+if __name__ == "__main__": 
+    import binascii
+    c = binascii.unhexlify(b'1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
+    print(find_single_byte_key(c))
--- Challenge6.py
+++ Challenge6.py
@@ -1,7 +1,41 @@
-def hamming_distance(s1, s2):
-    pass
+import Methods, math, base64, operator, Challenge3
 
+
+#Calculate hamming_distance of two strings
+def hamming_distance(s1, s2):
+    if len(s1) == len(s2):
+        #Convert string to bits
+        b1 = Methods.byteToBin(s1)
+        b2 = Methods.byteToBin(s2)
+        #Sum each matching bit               
+        return sum([int(b1[i])^int(b2[i]) for i in range(len(b1))])
+        
+def find_keysize(cipher, guess_from, guess_to):
+    distances = {}
+    for keysize in range(guess_from, guess_to):
+        normalized_distance = 0
+        #create different chunks for different keysize-guesses
+        chunks = [cipher[j*keysize:keysize*(j+1)] for j in range(math.ceil(len(cipher)/keysize))]
+        
+        normalized_distance += hamming_distance(chunks[0], chunks[1])/keysize
+        normalized_distance += hamming_distance(chunks[2], chunks[3])/keysize
+        normalized_distance += hamming_distance(chunks[1], chunks[3])/keysize
+        normalized_distance += hamming_distance(chunks[2], chunks[4])/keysize
+        normalized_distance += hamming_distance(chunks[0], chunks[4])/keysize
+        normalized_distance += hamming_distance(chunks[2], chunks[1])/keysize
+        
+        
+        distances.update({keysize:normalized_distance})
+        sorted_x = sorted(distances.items(), key=operator.itemgetter(1))
+    print(sorted_x)
+
+def find_key(cipher, keysize):
+    chunks = [cipher[]]
+        
 
 
 if __name__ == "__main__": 
-    pass
+    
+    cipher = base64.b64decode(open("Text6.txt").read())
+    find_keysize(cipher,2,40)
+    
